765767f96eb75827e055e80bcf998022da3fdb0b
saem <saem.hong.95@gmail.com>
2024-02-01 17:18:46 +0900

    first commit

---
+++ 00_javaBasic/src/edu/kh/basic/JavaTest.java
@@ -0,0 +1,28 @@
+package edu.kh.basic;
+
+//주석 
+public class JavaTest {
+	public static void main(String[] args){
+		System.out.println("Hello World!!!!!!");
+//		ctrl+alt+arrowkey == copyline
+		System.out.println("JavaTest");
+		System.out.println("100%");
+		System.out.println("syso+ctrl+space");
+		System.out.println("------------------------");
+//		숫자 연산
+		System.out.println("1+2");
+		System.out.println(1+2);
+//		"" == string
+//		"" != numb/bool
+		System.out.println(50-20);
+		System.out.println(50*2);
+		System.out.println(50/2);
+		System.out.println(12%5);
+//		str + num
+		System.out.println("14 * 19 = " + 14*19);
+		System.out.println("90+70+65=" + 90+70+65);
+		System.out.println("90+70+65=" + (90+70+65));
+//		num + num == 연산
+//		num + str / str + str == 이어쓰기
+	}
+}
